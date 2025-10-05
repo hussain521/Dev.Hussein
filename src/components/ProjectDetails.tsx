3e0dc38e1e73c99252c60abe2093550fa5cc08da
hussain521 <hussainelsid990@gmail.com>
2025-10-05 09:39:44 +0300

    fix bug

--- src/components/ProjectDetails.tsx
+++ src/components/ProjectDetails.tsx
@@ -57,17 +57,17 @@ const ProjectDetails = ({
           <img src="assets/close.svg" className="w-6 h-6" />
         </button>
         <CardContainer className="inter-var mx-2 md:mx-0 ">
-          <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
+          <CardBody className=" relative group/card  hover:shadow-2xl hover:shadow-emerald-500/[0.1] bg-black border-white/[0.2]  w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
             <CardItem
               translateZ="50"
-              className="text-xl font-bold text-neutral-600 dark:text-white"
+              className="text-xl font-bold text-white"
             >
               {title}
             </CardItem>
             <CardItem
               as="p"
               translateZ="60"
-              className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
+              className=" text-sm max-w-sm mt-2 text-neutral-300"
             >
               {description}
             </CardItem>
@@ -95,7 +95,7 @@ const ProjectDetails = ({
                 translateZ={20}
                 translateX={40}
                 as="a"
-                className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
+                className="px-4 py-2 rounded-xl text-xs font-normal text-white"
                 href={href}
               >
                 Try now →
